715a2b73162745073ecb0d2e2699d973412a5c59
Juan <cerecedajuanmanuel@gmail.com>
2023-11-12 00:16:40 -0300

    se adjunta la pagina Ticket más funcionalidades JS

--- index2.js
+++ index2.js
@@ -6,16 +6,12 @@ const descTrainee = 0.50;
 const descJunior= 0.15;
 
 //tomar los elementos
-
-const nombre= document.getElementsByClassName('form-control');
-const apellido = document.getElementsByClassName('form-control');
-const correo = document.getElementById('correo');
 const cantidad = document.getElementById('cantidad');
 const categoria= document.getElementById('categoria');
 const totalPagar= document.getElementById('TotalAPagar');
 const btnResumen = document.getElementById('btnResumen');
 const btnBorrar = document.getElementById('btnBorrar');
-console.log(totalPagar);
+// console.log(totalPagar);
 
 function calcularPago(){
     let total= cantidad.value * ticket;
@@ -28,17 +24,20 @@ function calcularPago(){
             total=total-(total*descTrainee);
         break;
         case "Junior":
-            total=total(total*descJunior);
+            total=total-(total*descJunior);
         break;
     }
     totalPagar.textContent = `Total a Pagar: $ ${total}`;
 
 }
 function borrar(){
-cantidad.value=" ";
-nombre.value= " ";
-apellido.value=" ";
-correo.value=" ";
+   const nombre= document.getElementById('nombre');
+   const apellido = document.getElementById('apellido'); 
+   const correo = document.getElementById('correo');
+nombre.value="";
+apellido.value="";
+correo.value="";
+cantidad.value = "";
 totalPagar.textContent=`Total a Pagar: $`;
 }
 // Evita refrescar lapagina
@@ -47,5 +46,6 @@ e.preventDefault();
 calcularPago();
 });
 btnBorrar.addEventListener('click',(e)=>{
+    e.preventDefault(); 
     borrar();
 });
